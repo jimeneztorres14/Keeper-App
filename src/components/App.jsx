da8d461fc0294c4e73ada246ddaec4601d2d209f
Your Name <rubenjimenez9412@gmail.com>
2024-09-21 16:29:26 -0500

    Complete Part 2 - Keeper Project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
+console.log(notes);
+
+function NotesHolder(data){
+    return(
+    <Note
+    key={data.key}
+    title={data.title}
+    content={data.content}
+    />)
+};
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map(NotesHolder)}
             <Footer />
         </div>
     )
